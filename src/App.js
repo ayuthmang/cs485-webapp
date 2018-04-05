86d63382e680bbcd69f46cdc8910a17dcd0a3d84
Ayuth Mangmesap <blackSource@windowslive.com>
2018-04-05 07:05:12 +0700

    Init sources

---
+++ src/App.js
@@ -0,0 +1,26 @@
+import React, { Component } from 'react';
+
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+import Header from './components/Header';
+import Footer from './components/Footer';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <Header/>
+
+        {/* Content Here */}
+
+
+
+        {/* Content Here */}
+
+        <Footer/>
+      </div>
+    );
+  }
+}
+
+export default App;
