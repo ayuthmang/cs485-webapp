fa28b84e71f7805698b76489ed4851f91f7bb3a2
Ayuth Mangmesap <ayuth.mang@gmail.com>
2018-04-08 01:29:33 +0700

    Add section highlight

--- src/components/Cardbox.js
+++ src/components/Cardbox.js
@@ -1,17 +1,20 @@
 import React from 'react';
-import { Card, CardImg, CardText, CardBody,
-  CardTitle, CardSubtitle, Button } from 'reactstrap';
+import {
+  Card, CardImg, CardText, CardBody,
+  CardTitle, CardSubtitle, Button
+} from 'reactstrap';
+import { CLIENT_RENEG_LIMIT } from 'tls';
 
 const Cardbox = (props) => {
   return (
     <div>
       <Card>
-        <CardImg top width="100%" src="https://placeholdit.imgix.net/~text?txtsize=33&txt=318%C3%97180&w=318&h=180" alt="Card image cap" />
+        <CardImg className="img-fluid img-thumbnail" top width="100%" src={props.img} alt="Card image cap" style={{ width: '100%', height: '360px' }}/>
         <CardBody>
-          <CardTitle>Card title</CardTitle>
-          <CardSubtitle>Card subtitle</CardSubtitle>
-          <CardText>Some quick example text to build on the card title and make up the bulk of the card's content.</CardText>
-          <Button>Button</Button>
+          <CardTitle>{props.cardTitle}</CardTitle>
+          <CardSubtitle>{props.cardSubtitle}</CardSubtitle>
+          <CardText>{props.cardText}</CardText>
+          {/* <Button color="primary">Button</Button> */}
         </CardBody>
       </Card>
     </div>
