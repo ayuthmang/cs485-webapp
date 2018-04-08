c78bba403a1dae836c7e8dbab95c028c955c0d7f
bommbibi <reborn_lovely_r.n@hotmail.com>
2018-04-08 11:08:57 +0700

    Boombi Toey Mint

---
+++ src/Zone.js
@@ -0,0 +1,9 @@
+const Zone = [
+  {Zone: 'Green Canteen', Bistro:[], stocked: true, name: 'Football', sku:'1234'},
+  {Zone: 'Torung', Bistro: ['พินิจโต้รุ่ง','อิ่มอร่อย','3 ป. ขาหมู อาหารตามสั่ง','อาหารอิสลาม Halal Food','ชาย 4 บะหมี่เกี๊ยว','ก๋วยเตี๋ยวแชมป์', 'ข้าวยำไก่ย่าง']},
+  {Zone: 'Sporting Goods', Bistro: '$29.99', stocked: false, name: 'Basketball', sku:'1344'},
+  {Zone: 'Chiang-Rak', Bistro: '$99.99', stocked: true, name: 'iPod Touch', sku:'3422'},
+  {Zone: 'Tuwson', Bistro: '$399.99', stocked: false, name: 'iPhone 5', sku:'2567'},
+];
+
+export default Zone;
