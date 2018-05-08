3a8027f779c4b10fd1b93e84204c72b803174470
Ayuth Mangmesap <blackSource@windowslive.com>
2018-05-08 17:19:02 +0700

    Delete all components and create header and footer

--- src/App.js
+++ src/App.js
@@ -1,34 +1,16 @@
 import React, { Component } from 'react';
-import { Container, Row, Col, Button } from 'reactstrap';
 
-import './styles/App.css';
-import 'bootstrap/dist/css/bootstrap.min.css';
 import Header from './components/Header';
 import Footer from './components/Footer';
-import Banner from './components/Banner';
-import Foodrecom from './components/Foodrecom';
-import PlaceHit from './components/PlaceHit';
-import ResHit from './components/ResHit';
-import Buy from './components/Buy';
+
+import 'semantic-ui-css/semantic.min.css';
+import './styles/App.css';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
         <Header />
-
-        {/* Content Here */}
-          <Container>
-          <Banner/>
-          {/* <PlaceHit/> */}
-          <ResHit/>
-          <Foodrecom/>
-          <Buy/>
-
-        </Container>
-
-        {/* Content Here */}
-
         <Footer />
       </div>
     );
