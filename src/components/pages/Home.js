a6a793355191f37e3ed3aba0e7fe84e8d04867ed
Ayuth Mangmesap <ayuth.mang@gmail.com>
2018-05-08 17:19:02 +0700

    Delete all components and create header and footer

---
+++ src/components/pages/Home.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import { Switch, Route } from 'react-router-dom';
+import { Link } from 'react-router-dom';
+
+export default class HomePage extends Component {
+  render() {
+    return (
+      <div>
+        {/* <h1> Hello World </h1> */}
+        {/* <Link to="/">Home</Link> */}
+      </div>
+    );
+  }
+}
