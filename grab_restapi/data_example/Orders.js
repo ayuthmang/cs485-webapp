5e5ba026d05dd1da98f7ddeb87b67ccc7976916f
Ayuth Mangmesap <ayuth.mang@gmail.com>
2018-05-14 02:42:52 +0700

    #3 add more attributes for sample data

--- grab_restapi/data_example/Orders.js
+++ grab_restapi/data_example/Orders.js
@@ -4,24 +4,30 @@ const data = {
   status_message: 'succeed',
   data: [
     {
+      title: 'ซื้อข้าวมันไก่ให้หน่อยยยยยยย หิวว้อยย',
+      description: 'ที่ไหนก็ได้ซื้อมา',
+      address: 'หอ C11 ห้อง 414',
       user_owner: 'ayuth',
       user_grabber: 'boombi',
       description: 'รายละเอียดของการซื้อ',
       status: 'pending', // pending, approved, reject
       price: 100,
-      tip: 10,
-      created_time: '2018-05-11 16:53:37.711',
-      updated_time: '2018-05-11 16:53:37.711'
+      tip: 10
     },
     {
+      title: 'อยากกินข้าวผัดกระเพรา',
+      description: 'ที่ไหนก็ได้ซื้อมา',
+      address: 'หอ C11 ห้อง 414',
       user_owner: 'ayuth',
       user_grabber: 'boombi',
       description: 'รายละเอียดของการซื้อ',
       status: 'approved', // pending, approved, reject
       price: 100,
-      tip: 10,
-      created_time: '2018-05-11 16:53:37.711',
-      updated_time: '2018-05-11 16:53:37.711'
+      tip: 10
     }
   ]
+};
+
+module.exports = {
+  data
 };
