d38b955bbc8bbca81e1fd063d17b275f55cbac88
Ayuth Mangmesap <blackSource@windowslive.com>
2018-05-13 21:58:31 +0700

    #3 finished with READ operations

---
+++ grab_restapi/controllers/orderController.js
@@ -0,0 +1,67 @@
+/*
+
+router.get('/orders', userController.getUsers);
+router.get('/order/:id', userController.getUsersByUsername);
+router.post('/order', userController.createUser);
+router.put('/order', userController.updateUser);
+router.delete('/order', userController.deleteUser);
+*/
+
+const Order = require('../models/orderModel');
+const validator = require('validator');
+
+// Retrieve and return all Users from the database.
+exports.getOrders = (req, res) => {
+  var orderProjection = {
+    __v: false,
+    _id: false
+  };
+  Order.find({}, orderProjection)
+    .then(orders => {
+      res.send({
+        status: true,
+        status_code: 200,
+        status_message: 'succeed',
+        data: orders
+      });
+    })
+    .catch(err => {
+      res.status(500).send({
+        status: false,
+        status_code: 500,
+        status_message: err.errmsg
+      });
+    });
+};
+
+exports.getOrderByUserOwner = (req, res) => {
+  const { username } = req.params;
+  var orderProjection = {
+    _id: false,
+    __v: false
+  };
+  Order.find({ user_owner: username }, orderProjection)
+    .then(orders => {
+      if (orders.legnth > 0) {
+        res.send({
+          status: true,
+          status_code: 200,
+          status_message: 'succeed',
+          data: orders
+        });
+      } else {
+        res.send({
+          status: false,
+          status_code: 200,
+          status_message: 'Order not found with username: ' + username
+        });
+      }
+    })
+    .catch(err => {
+      res.status(500).send({
+        status: false,
+        status_code: 500,
+        status_message: err.errmsg
+      });
+    });
+};
---
+++ grab_restapi/data_example/Users.js
@@ -0,0 +1,32 @@
+const data = [
+  {
+    username: 'admin',
+    password: '1234',
+    email: 'admin@dome.tu.ac.th',
+    status: 'approved'
+  },
+  {
+    username: 'ayuth',
+    password: '1234',
+    email: 'ayuth.man@dome.tu.ac.th',
+    status: 'approved'
+  },
+  {
+    username: 'kanlayanee',
+    password: '1234',
+    email: 'kanlayanee.dum@dome.tu.ac.th',
+    status: 'approved'
+  },
+  {
+    username: 'kansuda',
+    password: '1234',
+    email: 'kansuda.way@dome.tu.ac.th',
+    status: 'approved'
+  },
+  {
+    username: 'padchaya',
+    password: '1234',
+    email: 'padchaya.sri@dome.tu.ac.th',
+    status: 'approved'
+  }
+];
